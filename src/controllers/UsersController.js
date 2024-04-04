a62d83d0eda716e1f09392acd83fe0ee2cf4b2ca
Meinke7 <carlos.meinke7@gmail.com>
2024-04-04 10:49:54 -0300

    add user controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -17,58 +17,16 @@ class UsersController {
 
         await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
 
-<<<<<<< HEAD
-
-async update(request, response) {
-  const { name, email, password, old_password } = request.body
-  const user_id = request.user.id
-
-  const database = await sqliteConnection()
-  const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
-
-  if (!user) {
-    throw new AppError("Usuário não encontrado.")
-  }
-
-  const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
-
-  if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
-    throw new AppError("Este email já está em uso.")
-  }
-
-  user.name = name ?? user.name
-  user.email = email ?? user.email
-
-  if (password && ! old_password) {
-    throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
-  }
-
-  if (password && old_password) {
-    const checkOldPassword = await compare(old_password, user.password)
-    if(!checkOldPassword) {
-      throw new AppError("A senha antiga não confere.")
-=======
-        return response.status(201).json();
->>>>>>> 70e9cf2972515484462a0360273f9bb08c6dd653
+        return response.status(201).json({ message: "Usuário criado com sucesso." });
     }
 
     async update(request, response) {
         const { name, email, password, old_password } = request.body;
         const { id } = request.params;
+        const user_id = request.user.id;
 
-<<<<<<< HEAD
-  await database.run(`
-  UPDATE users SET
-  name = ?,
-  email = ?,
-  password = ?,
-  updated_at = DATETIME('now')
-  WHERE id = ?`,
-  [user.name, user.email, user.password, user_id])
-=======
         const database = await sqliteConnection();
-        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
->>>>>>> 70e9cf2972515484462a0360273f9bb08c6dd653
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
 
         if (!user) {
             throw new AppError("Usuário não encontrado.");
@@ -79,9 +37,6 @@ async update(request, response) {
         if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
             throw new AppError("Este e-mail já está em uso.");
         }
-
-        user.name = name ?? user.name;
-        user.email = email ?? user.email;
 
         if (password && !old_password) {
             throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
@@ -103,10 +58,10 @@ async update(request, response) {
             password = ?,
             updated_at = DATETIME('now')
             WHERE id = ?`,
-            [user.name, user.email, user.password, id]
+            [name || user.name, email || user.email, user.password, id]
         );
 
-        return response.status(200).json();
+        return response.status(200).json({ message: "Usuário atualizado com sucesso." });
     }
 }
 
